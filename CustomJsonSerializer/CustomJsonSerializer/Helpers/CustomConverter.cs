cbd5e46ac179c71cb5f4fe5a522d4033005fcbd9
RasulPirsoltanov <7m0uvuf@code.edu.az>
2023-12-25 21:57:54 +0400

    finished CustomConverter

---
+++ CustomJsonSerializer/CustomJsonSerializer/Helpers/CustomConverter.cs
@@ -0,0 +1,86 @@
+﻿using CustomJsonSerializer.Models;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+
+namespace CustomJsonSerializer.Extensions
+{
+    public static class CustomConverter
+    {
+        /*<summary>
+         T is generic
+        </summary>*/
+        public static T Deserialize<T>(string value) where T : class, new()
+        {
+            try
+            {
+                JObject jObject = JObject.Parse(value);
+                T rootClass = new T();
+
+                foreach (JProperty property in jObject.Properties())
+                {
+                    PropertyInfo targetProperty = rootClass.GetType().GetProperty(property.Name);
+
+                    if (targetProperty != null)
+                    {
+                        targetProperty.SetValue(rootClass, property.Value.ToObject(targetProperty.PropertyType));
+                    }
+                }
+                return rootClass;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+
+        public static string Serializer<T>(T root) where T : class, new()
+        {
+            string newJson = "{\n";
+            foreach (var item in root.GetType().GetProperties())
+            {
+                if (!item.PropertyType.IsGenericType && item.PropertyType == typeof(string))
+                {
+                    newJson += '"' + item.Name + '"' + ':' + '"' + item.GetValue(root) + '"' + ',' + "\n";
+                }
+                else if (item.PropertyType.IsGenericType)
+                {
+                    var genericValue = item.GetValue(root);
+                    newJson += '"' + item.Name + '"' + ':' + "[";
+                    foreach (var gv in genericValue as IEnumerable<object>)
+                    {
+                        newJson += '"' + gv.ToString() + '"' + ',';
+                    }
+                    if (newJson.Last() == ',')
+                    {
+                        newJson = newJson.Remove(newJson.Length - 1);
+                    }
+                    newJson += "]" + ',' + "\n";
+                    Console.WriteLine("\n\nGenerics");
+                    Console.WriteLine(item.GetValue(root));
+                }
+                else if (item.PropertyType != typeof(string) && !item.PropertyType.IsGenericType)
+                {
+                    var genericValue = item.GetValue(root);
+                    newJson += '"' + item.Name + '"' + ':' + "{";
+                    foreach (var gv in genericValue.GetType().GetProperties())
+                    {
+                        newJson += '"' + gv.Name + '"' + ":" + '"' + gv.GetValue(genericValue) + '"' + ',';
+                    }
+                    if (newJson.Last() == ',')
+                    {
+                        newJson = newJson.Remove(newJson.Length - 1);
+                    }
+                    newJson += "}" + ',' + "\n";
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                }
+
+            }
+            newJson += "}";
+            return newJson;
+        }
+    }
+}
---
+++ CustomJsonSerializer/CustomJsonSerializer/Program.cs
@@ -0,0 +1,39 @@
+﻿using CustomJsonSerializer.Extensions;
+using CustomJsonSerializer.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CustomJsonSerializer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var json = @"{""isbn"": ""123-456-222"",  
+ ""author"": 
+    {
+      ""lastname"": ""Doe"",
+      ""firstname"": ""Jane""
+    },
+""editor"": 
+    {
+      ""lastname"": ""Smith"",
+      ""firstname"": ""Jane""
+    },
+  ""title"": ""The Ultimate Database Study Guide"",    ""titasale"": ""The Ultimate Database Study Guide"",  
+
+  ""category"": [""Non-Fiction"", ""Technology""]
+ }";
+
+
+            Root root = CustomConverter.Deserialize<Root>(json);
+            string newJson = CustomConverter.Serializer<Root>(root);
+            Console.WriteLine("\nNew Json\n" + newJson);
+        }
+
+    }
+}
